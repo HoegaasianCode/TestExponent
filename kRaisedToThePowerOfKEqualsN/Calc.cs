16aff9384ad4397c1cd555463f4bec79055a006d
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:46:30 +0100

    Initial commit

---
+++ kRaisedToThePowerOfKEqualsN/Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace kRaisedToThePowerOfKEqualsN
+{
+    public class Calc
+    {
+        private readonly int _k;
+        private readonly string _n;
+
+        public Calc(string n, int k)
+        {
+            _n = n;
+            _k = k;
+        }
+
+        public bool IsSquare()
+        {
+            var result = Double.Parse(_n);
+            return Math.Sqrt(result) == _k;
+        }
+    }
+}
---
+++ kRaisedToThePowerOfKEqualsN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kRaisedToThePowerOfKEqualsN
+{
+    class Program
+    {
+        // https://edabit.com/challenge/jxQTbif5fx7J8FeTT
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var x = new Calc("4", 2);
+            Console.Write(x.IsSquare());
+        }
+    }
+}
